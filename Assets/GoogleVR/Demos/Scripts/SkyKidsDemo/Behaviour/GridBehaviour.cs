83ab1519ad14430e216676c1a8a9f1258f6af081
lukeworgerSky <luke.worger@sky.uk>
2017-07-30 11:30:42 +0100

    added dynamic selected product tab shown

---
+++ Assets/GoogleVR/Demos/Scripts/SkyKidsDemo/Behaviour/GridBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridBehaviour : MonoBehaviour {
+
+	GameObject[] gridButtons;
+	public GameObject selectedProductPanel;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void OnEnable()
+	{
+		Debug.Log ("GRID BEHAVIOUR ENABLED WAS CALLED");
+		gridButtons = GameObject.FindGameObjectsWithTag ("productButton");
+		Debug.Log ("length from enable: " +gridButtons.Length);
+		foreach(GameObject gridButtonObject in gridButtons){
+
+			string parentName = gridButtonObject.transform.parent.name;
+			Debug.Log ("ParentName:  " + parentName);
+			Button gridButton = gridButtonObject.GetComponent<UnityEngine.UI.Button>();
+
+			gridButton.onClick.AddListener(delegate { setSelectedItem(parentName); });
+
+		}
+	}
+
+
+	public void setSelectedItem(string parentName){
+		Debug.Log ("Set selected item called!!!!!");
+		selectedProductPanel.SetActive (true);
+		GameObject selectedName = GameObject.Find ("selectedProductText");
+		Text selectedNameText = selectedName.GetComponent<UnityEngine.UI.Text> ();
+		selectedNameText.text = parentName;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
